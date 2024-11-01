d21be2ee577ccbab6de937325d8c5df80a078177
Brown_ Alexander James <alexander.brown@cn144.mgmt.kamiak.wsu.edu>
2024-10-31 20:48:50 -0700

    feat: data normalized in preparation for differential expression anaylsis

--- src/app/differential_expression_analysis.py
+++ src/app/differential_expression_analysis.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser
-import pandas as pd
+import pandas as pd # type: ignore
 from pathlib import Path
-from pydeseq2.dds import DeseqDataSet
+from pydeseq2.dds import DeseqDataSet # type: ignore
 
 class SampleMetaDataManager:
     def __init__(self, metadata_path: Path) -> None:
@@ -12,6 +12,7 @@ class SampleMetaDataManager:
         sample_metadata = self.remove_rows_without_id(sample_metadata)
         sample_metadata = self.set_gene_count_id(sample_metadata)
         sample_metadata = self.set_sample_id_as_index(sample_metadata)
+        sample_metadata = self.sort_by_index(sample_metadata)
         self.sample_metadata = sample_metadata
         
         self.count_id_conversion = self.set_count_id_to_sample_id_dict(self.sample_metadata)
@@ -41,6 +42,10 @@ class SampleMetaDataManager:
         return sample_metadata.set_index("Sample ID")
     
     @staticmethod
+    def sort_by_index(sample_metadata: pd.DataFrame) -> pd.DataFrame:
+        return sample_metadata.sort_index()
+    
+    @staticmethod
     def set_count_id_to_sample_id_dict(sample_metadata: pd.DataFrame) -> dict[str]:
         gene_count_ids = sample_metadata["gene_count_id"].tolist()
         sample_ids = sample_metadata.index.tolist()
@@ -55,7 +60,8 @@ class GeneCountManager:
         gene_counts = self.rename_gene_count_ids(gene_counts, count_id_conversion)
         gene_counts = self.set_gene_id_as_index(gene_counts)
         gene_counts = self.remove_all_zero_rows(gene_counts)
-        self.gene_counts = self.transpose_gene_counts(gene_counts)
+        gene_counts = self.transpose_gene_counts(gene_counts)
+        self.gene_counts = self.sort_by_index(gene_counts)
 
     @staticmethod
     def extract_gene_counts(gene_counts_path: Path) -> pd.DataFrame:
@@ -78,6 +84,33 @@ class GeneCountManager:
     def transpose_gene_counts(gene_counts: pd.DataFrame) -> pd.DataFrame:
         return gene_counts.T
 
+    @staticmethod
+    def sort_by_index(gene_counts: pd.DataFrame) -> pd.DataFrame:
+        return gene_counts.sort_index()
+
+class DifferentialExpressionAnalysis:
+    def __init__(self):
+        pass
+    
+    def run(self, gene_counts: pd.DataFrame, sample_metadata: pd.DataFrame) -> None:
+        gene_counts, sample_metadata = self.remove_discrepant_sample_ids(gene_counts, sample_metadata)
+        design_factors = ["Treatment", "Cell Line", "Time", "Lib. Prep Batch"] # Need to add virus back without NA
+        dds = DeseqDataSet(counts=gene_counts,
+                           metadata=sample_metadata,
+                           design_factors=design_factors)
+        dds.deseq2()
+        print(dds)
+        
+    @staticmethod
+    def remove_discrepant_sample_ids(gene_counts: pd.DataFrame, sample_metadata: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+        gene_count_sample_ids = set(gene_counts.index)
+        metadata_sample_ids = set(sample_metadata.index)
+        shared_ids = gene_count_sample_ids & metadata_sample_ids
+
+        gene_counts = gene_counts[gene_counts.index.isin(shared_ids)]
+        sample_metadata = sample_metadata[sample_metadata.index.isin(shared_ids)]
+        return gene_counts, sample_metadata
+
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-c", "--counts", type=str, required=True)
@@ -89,3 +122,6 @@ if __name__ == "__main__":
 
     gcm = GeneCountManager(Path(args.counts))
     gcm.run(smdm.count_id_conversion)
+    
+    dea = DifferentialExpressionAnalysis()
+    dea.run(gcm.gene_counts, smdm.sample_metadata)
