744c4753701977d81a3808e91434a993d3135cb4
Brown_ Alexander James <alexander.brown@login-p3n01.kamiak.wsu.edu>
2024-11-12 13:29:40 -0800

    feat: R script to perform LRT to account for effect of time

--- src/app/log_ratio_test.R
+++ src/app/log_ratio_test.R
@@ -1,19 +1,32 @@
-print("Starting")
-library(DESeq2)
+args = commandArgs(trailingOnly=TRUE)
 
-gene_count_path <- "/home/alexander.brown/zika-rnaseq-analysis/src/data/pydeseq2/gene_counts_R_input.csv"
-sample_metadata_path <- "/home/alexander.brown/zika-rnaseq-analysis/src/data/pydeseq2/sample_metadata_R_input.csv"
+suppressMessages(library(DESeq2))
 
-gene_counts=read.csv(gene_count_path, sep=",", head=T, row.names="X")
+gene_count_path <- args[1]
+sample_metadata_path <- args[2]
+lrt_results_outpath <- args[3]
+
+# Load data
+gene_counts=t(read.csv(gene_count_path, sep=",", head=T, row.names="X"))
 sample_metadata=read.csv(sample_metadata_path, sep=",", head=T, row.names="Sample.ID")
 
+# Define metadata factors for modelling
 sample_metadata$Time <- factor(sample_metadata$Time)
 sample_metadata$Virus <- factor(sample_metadata$Virus)
-#head(sample_metadata, n=c(20,6))
 
+# Defining models
 full_model <- ~ Virus + Time + Virus:Time
 reduced_model <- ~ Virus + Time
 
-print(full_model)
+# Creating DESeqDataSet object
+dds <- DESeqDataSetFromMatrix(countData = gene_counts,
+                              colData = sample_metadata, 
+                              design = full_model)
 
-print("Done")
+# Performing Likelihood Ratio Test to account for effect of time
+dds_lrt_time <- DESeq(dds, test="LRT", reduced=reduced_model)
+
+results_lrt <- results(dds_lrt_time)
+
+# Write results to csv
+write.csv(results_lrt, lrt_results_outpath, row.names=TRUE, quote=FALSE)
