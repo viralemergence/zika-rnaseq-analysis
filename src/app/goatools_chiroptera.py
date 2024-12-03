3e4e371905f228e6fe7c715188be227d8115f965
Brown_ Alexander James <alexander.brown@login-p3n03.kamiak.wsu.edu>
2024-12-03 14:06:52 -0800

    feat: added taxon specific gene2go file creation to reduce runtime

--- src/app/goatools_chiroptera.py
+++ src/app/goatools_chiroptera.py
@@ -1,32 +1,63 @@
 from collections import defaultdict
 from csv import reader
-from goatools.anno.genetogo_reader import Gene2GoReader
-from goatools.base import dnld_file
-from goatools.go_enrichment import GOEnrichmentStudy
-from goatools.mapslim import mapslim
-from goatools.obo_parser import GODag
+from goatools.anno.genetogo_reader import Gene2GoReader # type: ignore
+from goatools.base import dnld_file # type: ignore
+from goatools.go_enrichment import GOEnrichmentStudy # type: ignore
+from goatools.mapslim import mapslim # type: ignore
+from goatools.obo_parser import GODag # type: ignore
 from pathlib import Path
 
 class GoatoolsManager:
-    def __init__(self) -> None:
+    def __init__(self, taxon_id: int) -> None:
+        # Background and study genes for enrichment analysis
         self.background_genes_path = Path("/src/data/go_analysis/ncbi_gene_results.txt")
+        self.study_genes_path = Path("/src/data/pydeseq2/degpatterns/gene_clusters.csv")
+
+        # Full and slim GO terms in obo format
         self.obo_ftp_path = "http://current.geneontology.org/ontology/go.obo"
         self.obo_path = Path("/src/data/go_analysis/go.obo")
-        self.slim_obo_ftp_path = "https://current.geneontology.org/ontology/subsets/goslim_agr.obo"
-        self.slim_obo_path = Path("/src/data/go_analysis/goslim-agr.obo")
+        self.slim_obo_ftp_path = "https://current.geneontology.org/ontology/subsets/goslim_pir.obo"
+        self.slim_obo_path = Path("/src/data/go_analysis/goslim-pir.obo")
+
+        # gene2go and taxon specific gene2go info
         self.gene2go_ftp_path = "ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene2go.gz"
         self.gene2go_path = Path("/src/data/go_analysis/gene2go.txt")
-        self.study_genes_path = Path("/src/data/pydeseq2/degpatterns/gene_clusters.csv")
+        self.taxon_id = taxon_id
+        self.gene2go_taxon_path = Path(f"/src/data/go_analysis/gene2go_{self.taxon_id}.txt")
+
+        # Final results path
         self.outpath = Path("/src/data/go_analysis/goatools_results.csv")
-        
-        self.taxon_id = 9407
 
     def setup(self) -> None:
+        if not self.gene2go_taxon_path.is_file():
+            dnld_file(self.gene2go_ftp_path, str(self.gene2go_path))
+            self.filter_gene2go_by_taxon_id(self.gene2go_path, self.gene2go_taxon_path, self.taxon_id)
         dnld_file(self.obo_ftp_path, str(self.obo_path))
-        dnld_file(self.gene2go_ftp_path, str(self.gene2go_path))
         dnld_file(self.slim_obo_ftp_path, str(self.slim_obo_path))
         self.get_background_genes(self.background_genes_path)
         
+    @staticmethod
+    def filter_gene2go_by_taxon_id(gene2go_path: Path, gene2go_taxon_path: Path, taxon_id: int) -> None:
+        # Creating a taxon specific gene2go file dramatically increases Gene2GoReader load time
+        with gene2go_path.open() as inhandle, gene2go_taxon_path.open("w") as outhandle:
+            reader_iterator = reader(inhandle, delimiter="\t")
+            header = next(reader_iterator)
+            outhandle.write("\t".join(header) + "\n")
+            for line in reader_iterator:
+                tax_id = int(line[0])
+                if tax_id != taxon_id:
+                    continue
+                outhandle.write("\t".join(line) + "\n")
+
+    @staticmethod
+    def get_background_genes(background_genes_path: Path) -> None:
+        # NOTE: This is a WIP
+        # Currenty instructions are:
+        # Go to: https://www.ncbi.nlm.nih.gov/gene
+        # For Rousettus search: "9407"[Taxonomy ID] AND alive[property] AND genetype protein coding[Properties]
+        # Send to file (must be tabular)
+        pass
+
     def run(self) -> None:
         background_genes = self.extract_background_genes(self.background_genes_path)
         symbol_id_mapper = self.set_symbol_id_mapper(self.background_genes_path)
@@ -37,7 +68,7 @@ class GoatoolsManager:
         godag = GODag(str(self.obo_path), optional_attrs=["relationship"])
         goslim_dag = GODag(str(self.slim_obo_path), optional_attrs=["relationship"])
 
-        objanno = Gene2GoReader(str(self.gene2go_path), taxids=[self.taxon_id])
+        objanno = Gene2GoReader(str(self.gene2go_taxon_path), taxids=[self.taxon_id])
         ns2assoc = objanno.get_ns2assc()
         id2gos = defaultdict(set)
         for _, associations in ns2assoc.items():
@@ -73,15 +104,6 @@ class GoatoolsManager:
                 info = [r.GO, r.goterm.name, round(r.p_uncorrected, 5), round(r.p_fdr_bh, 5), r.ratio_in_study[0], r.ratio_in_study[1], go_genes, all_ancestors]
                 info = ",".join([str(s) for s in info])
                 outhandle.write(info + "\n")
-
-    @staticmethod
-    def get_background_genes(background_genes_path: Path) -> None:
-        # NOTE: This is a WIP
-        # Currenty instructions are:
-        # Go to: https://www.ncbi.nlm.nih.gov/gene
-        # For Rousettus search: "9407"[Taxonomy ID] AND alive[property] AND genetype protein coding[Properties]
-        # Send to file (must be tabular)
-        pass
     
     @staticmethod
     def extract_background_genes(background_genes_path: Path) -> set[str]:
@@ -130,20 +152,8 @@ class GoatoolsManager:
                 continue
         return converted_study_genes
 
-    @staticmethod
-    def check_gene2go_id_count(gene2go_path: Path, taxid: str) -> None:
-        gene_ids = set()
-        with gene2go_path.open() as inhandle:
-            reader_iterator = reader(inhandle, delimiter="\t")
-            header = next(reader_iterator)
-            for line in reader_iterator:
-                if line[0] != taxid:
-                    continue
-                gene_id = line[1]
-                gene_ids.add(gene_id)
-        print(len(gene_ids))
-
 if __name__ == "__main__":
-    gm = GoatoolsManager()
+    taxon_id = 9407
+    gm = GoatoolsManager(taxon_id)
     gm.setup()
     gm.run()
