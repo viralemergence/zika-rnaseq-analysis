aec0d762074b21a5aadfc467a5f49b1c4a96e70c
Brown_ Alexander James <alexander.brown@login-p3n01.kamiak.wsu.edu>
2024-12-11 17:14:02 -0800

    fix: removed gomcl installation from Dockerfile, and added array slurm job for goatools

---
+++ kamiak/goatools_array_prv.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --partition=remi
+#SBATCH --job-name=goatools_array
+#SBATCH --output=%x_%A_%a.out
+#SBATCH --error=%x_%A_%a.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=alexander.brown@wsu.edu
+#SBATCH --time=1-00:00:00
+#SBATCH --array=0-2:1,4,6,8,12%10
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=10G
+
+SINGULARITY_IMAGE="/home/alexander.brown/zika-rnaseq-analysis/src/singularity_image/zika.sif"
+
+HOST_APP_DIR="/home/alexander.brown/zika-rnaseq-analysis/src/app"
+
+HOST_DATA_DESEQ_DIR="/home/alexander.brown/zika-rnaseq-analysis/src/data/pydeseq2"
+HOST_DATA_GO_DIR="/home/alexander.brown/zika-rnaseq-analysis/src/data/go_analysis"
+
+module load singularity
+singularity exec --pwd /src \
+    --no-home \
+    --bind $HOST_APP_DIR:/src/app \
+    --bind $HOST_DATA_DESEQ_DIR:/src/data/pydeseq2 \
+    --bind $HOST_DATA_GO_DIR:/src/data/go_analysis \
+    $SINGULARITY_IMAGE \
+    python3 -u /src/app/goatools_analysis.py \
+    -t 9407 -g $SLURM_ARRAY_TASK_ID \
+    -b "/src/data/go_analysis/ncbi_gene_results_9407.txt" \
+    -s "/src/data/pydeseq2/degpatterns/R06E_PRV_gene_clusters.csv"
