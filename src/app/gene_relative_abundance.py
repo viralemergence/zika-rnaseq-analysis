65c24e80e819c72f85f82e49905db34f173c0c25
Brown_ Alexander James <alexander.brown@login-p3n03.kamiak.wsu.edu>
2024-12-19 11:31:37 -0800

    feat: removed relative abundance LRT results dependency and iterates through gene groups

--- src/app/gene_relative_abundance.py
+++ src/app/gene_relative_abundance.py
@@ -18,45 +18,26 @@ def suppress_stdout_stderr():
             yield(out, err)
 
 class GeneRelativeAbundance:
-    def __init__(self, genes_path: Path, cluster_path: Path):
+    def __init__(self, genes_path: Path):
         self.genes_of_interest = self.extract_genes_of_interest(genes_path)
         self.cell_line = genes_path.stem.split("_")[0]
         self.virus = genes_path.stem.split("_")[1]
-        self.clusters = self.extract_clusters(cluster_path)
 
     @staticmethod
     def extract_genes_of_interest(genes_path: Path, header: bool = True) -> list[str]:
-        goi = []
+        groups = defaultdict(list)
         with genes_path.open() as inhandle:
-            reader_iterator = reader(inhandle)
-            if header:
-                header_line = next(reader_iterator)
-                for line in reader_iterator:
-                    try:
-                        if float(line[-1]) > 0.05:
-                            continue
-                    except ValueError:
-                        continue
-                    goi.append(line[0])
-        return goi
-
-    @staticmethod
-    def extract_clusters(cluster_path: Path) -> defaultdict[list[str]]:
-        clusters = defaultdict(list)
-        with cluster_path.open() as inhandle:
             reader_iterator = reader(inhandle)
             header = next(reader_iterator)
             for line in reader_iterator:
-                clusters[int(line[1])].append(line[0])
-        return clusters
+                groups[str(line[1])].append(line[0])
+        return groups
 
     def run(self, gene_counts: pd.DataFrame, sample_metadata: pd.DataFrame) -> None:
-        print(len(self.genes_of_interest))
-        design_factors = ["Time", "Virus"] # Doesn't like Virus AND Treatment (redundancy probably)
+        design_factors = ["Time", "Virus"]
         print(f"Cell line: {self.cell_line}\nVirus: {self.virus}")
 
         gene_counts_by_cell_line, sample_metadata_by_cell_line = self.filter_for_cell_line(gene_counts, sample_metadata, self.cell_line)
-
         gene_counts_by_virus, sample_metadata_by_virus = self.filter_for_virus(gene_counts_by_cell_line, sample_metadata_by_cell_line, self.virus)
 
         print("Starting pyDEseq2 analysis") # TODO: Could put all this in a "differential expression" function for easier reading
@@ -70,40 +51,32 @@ class GeneRelativeAbundance:
 
         normalized_counts = self.extract_normalized_count_df_from_dds(dds)
         normalized_counts, sample_metadata_by_virus = self.remove_zero_time_point(normalized_counts, sample_metadata_by_virus)
-        genes_of_interest = self.filter_genes_of_interest(self.genes_of_interest, normalized_counts)
         
-        normalized_counts_of_interest = normalized_counts[genes_of_interest].copy().applymap(lambda x: log2(x + 1)) # NOTE: May not want 0 correction
-        normalized_counts_of_interest["Time"] = dds.obs["Time"].astype(float).astype(int)
-        normalized_counts_of_interest["Virus"] = dds.obs["Virus"]
-        
-        grouped_counts = normalized_counts_of_interest.groupby(["Time", "Virus"])
-        grouped_replicate_means = grouped_counts.mean().round(2)
+        for group, genes in self.genes_of_interest.items():
+            genes_of_interest = self.filter_genes_of_interest(genes, normalized_counts)
+            
+            normalized_counts_of_interest = normalized_counts[genes_of_interest].copy().applymap(lambda x: log2(x + 1)) # NOTE: May not want 0 correction
+            normalized_counts_of_interest["Time"] = dds.obs["Time"].astype(float).astype(int)
+            normalized_counts_of_interest["Virus"] = dds.obs["Virus"]
+            
+            grouped_counts = normalized_counts_of_interest.groupby(["Time", "Virus"])
+            grouped_replicate_means = grouped_counts.mean().round(2)
 
-        gene_stats = grouped_replicate_means.aggregate(["mean", "std"], axis=0).round(2)
+            gene_stats = grouped_replicate_means.aggregate(["mean", "std"], axis=0).round(2)
 
-        relative_abundance_zscores = grouped_replicate_means.copy()
-        for gene in grouped_replicate_means.keys():
-            mean = gene_stats.loc["mean", gene]
-            std = gene_stats.loc["std", gene]
-            relative_abundance_zscores[gene] = relative_abundance_zscores[gene].apply(lambda x: (x-mean)/std)
+            relative_abundance_zscores = grouped_replicate_means.copy()
+            for gene in grouped_replicate_means.keys():
+                mean = gene_stats.loc["mean", gene]
+                std = gene_stats.loc["std", gene]
+                relative_abundance_zscores[gene] = relative_abundance_zscores[gene].apply(lambda x: (x-mean)/std)
 
-        cluster_genes = self.clusters[1]
-        cluster_zscores = relative_abundance_zscores[cluster_genes]
-
-        def percentile(percentile_threshold: float):
-            def percentile_(series: pd.Series):
-                return series.quantile(percentile_threshold)
-            percentile_.__name__ = f"percentile_{percentile_threshold*100}"
-            return percentile_
-        
-        cluster_zscore_stats = cluster_zscores.aggregate(["median", "std", percentile(0.25), percentile(0.50), percentile(0.75)], axis=1).round(2)
-        print(cluster_zscore_stats)
-
-    @staticmethod
-    def extract_cell_lines(sample_metadata: pd.DataFrame) -> set[str]:
-        cell_lines = set(sample_metadata["Cell Line"].to_list())
-        cell_lines.discard("nan")
-        return cell_lines
+            def percentile(percentile_threshold: float):
+                def percentile_(series: pd.Series):
+                    return series.quantile(percentile_threshold)
+                percentile_.__name__ = f"percentile_{percentile_threshold*100}"
+                return percentile_
+            
+            zscore_stats = relative_abundance_zscores.aggregate(["median", "std", percentile(0.25), percentile(0.50), percentile(0.75)], axis=1).round(2)
     
     @staticmethod
     def filter_for_cell_line(gene_counts: pd.DataFrame, sample_metadata: pd.DataFrame, cell_line: str) -> tuple[pd.DataFrame, pd.DataFrame]:
@@ -117,6 +90,13 @@ class GeneRelativeAbundance:
         sample_metadata = sample_metadata[sample_metadata["Virus"].isin(virus_list)]
         gene_counts = gene_counts[gene_counts.index.isin(sample_metadata.index)]
         return gene_counts, sample_metadata
+
+    @staticmethod
+    def extract_normalized_count_df_from_dds(dds: DeseqDataSet) -> pd.DataFrame:
+        normalized_counts = dds.layers["normed_counts"]
+        gene_ids = dds._var.index.to_list()
+        sample_ids = dds.obsm["design_matrix"].index.to_list()
+        return pd.DataFrame(normalized_counts, index=sample_ids, columns=gene_ids)
 
     @classmethod
     def remove_zero_time_point(cls, gene_counts: pd.DataFrame, sample_metadata: pd.DataFrame) -> tuple[pd.DataFrame]:
@@ -132,6 +112,18 @@ class GeneRelativeAbundance:
         gene_counts = gene_counts[gene_counts.index.isin(shared_ids)]
         sample_metadata = sample_metadata[sample_metadata.index.isin(shared_ids)]
         return gene_counts, sample_metadata
+
+    @staticmethod
+    def filter_genes_of_interest(genes_of_interest: list[str], normalized_counts: pd.DataFrame) -> list[str]:
+        filtered_genes_of_interest = []
+        missing_genes_of_interest = []
+        for gene in genes_of_interest:
+            if gene in normalized_counts.columns:
+                filtered_genes_of_interest.append(gene)
+            else:
+                missing_genes_of_interest.append(gene)
+        print(f"Missing the following GOI: {missing_genes_of_interest}")
+        return filtered_genes_of_interest
 
     @classmethod
     def gene_of_interest_graphs(cls, cell_line: str, virus: str, genes_of_interest: list[str], dds: DeseqDataSet) -> None:
@@ -155,35 +147,15 @@ class GeneRelativeAbundance:
             plt.savefig(figure_outpath, bbox_inches="tight")
             plt.close()
 
-    @staticmethod
-    def extract_normalized_count_df_from_dds(dds: DeseqDataSet) -> pd.DataFrame:
-        normalized_counts = dds.layers["normed_counts"]
-        gene_ids = dds._var.index.to_list()
-        sample_ids = dds.obsm["design_matrix"].index.to_list()
-        return pd.DataFrame(normalized_counts, index=sample_ids, columns=gene_ids)
-
-    @staticmethod
-    def filter_genes_of_interest(genes_of_interest: list[str], normalized_counts: pd.DataFrame) -> list[str]:
-        filtered_genes_of_interest = []
-        missing_genes_of_interest = []
-        for gene in genes_of_interest:
-            if gene in normalized_counts.columns:
-                filtered_genes_of_interest.append(gene)
-            else:
-                missing_genes_of_interest.append(gene)
-        print(f"Missing the following GOI: {missing_genes_of_interest}")
-        return filtered_genes_of_interest
-
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-c", "--counts", type=str, required=True)
     parser.add_argument("-m", "--metadata", type=str, required=True)
     parser.add_argument("-g", "--genes", type=str, required=True)
-    parser.add_argument("-k", "--clusters", type=str, required=True)
     args = parser.parse_args()
 
     gene_counts = pd.read_csv(args.counts, index_col=0)
     sample_metadata = pd.read_csv(args.metadata, index_col=0)
     
-    gra = GeneRelativeAbundance(Path(args.genes), Path(args.clusters))
+    gra = GeneRelativeAbundance(Path(args.genes))
     gra.run(gene_counts, sample_metadata)
